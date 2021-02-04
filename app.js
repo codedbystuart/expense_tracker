1c3a663de1b3855eae280c4ce30b69bea1a36d4a
codedbystuart <code.stuartdambi@gmail.com>
2021-02-04 14:38:42 +0300

    [chore]: setup es6 use of import and export

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const express = require('express');
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => { console.log(`app is running on port ${port}`) });
