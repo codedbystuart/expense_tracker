b51d8a7b3a9d6c95cf977bb6217ba1971e8c6091
codedbystuart <code.stuartdambi@gmail.com>
2021-02-12 16:05:41 +0300

    [clean]: remove console log

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ import express from 'express';
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
 app.get('/', (req, res) => res.status(200).send({ message: 'Hello there' }));
 
-app.listen(port, () => { console.log(`app is running on port ${port}`); });
+const port = process.env.PORT || 5000;
+app.listen(port);
 
 export default app;
