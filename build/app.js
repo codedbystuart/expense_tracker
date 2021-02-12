58e4c6823e0a50323aff4defbcfc04b992c7c35c
codedbystuart <code.stuartdambi@gmail.com>
2021-02-12 16:05:09 +0300

    [add]: build files

--- build/app.js
+++ build/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
@@ -8,12 +12,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const app = (0, _express2.default)();
 
-const port = process.env.PORT || 5000;
-
 app.get('/', (req, res) => res.status(200).send({ message: 'Hello there' }));
 
-app.listen(port, () => {
-  console.log(`app is running on port ${port}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-module.exports = app;
+exports.default = app;
